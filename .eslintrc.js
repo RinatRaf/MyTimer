e711477d063c5aef633a88a85c6650db3e1f8f51
Khamitov Ruslan <hamitovra@lexema.ru>
2024-03-10 15:51:10 +0500

    fix lint, restrict default imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
   root: true,
-  extends: "@react-native",
-  singleQuote: true,
+  extends: ['@react-native', 'plugin:import/recommended'],
+  rules: {
+    'import/no-default-export': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    'import/ignore': ['react-native'],
+  },
 };
